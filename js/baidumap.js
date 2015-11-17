5db294a745e6727de4b20116cadd75d348eed7c1
Jason <jason.wei@jipart.com>
2015-11-17 17:48:34 +0800

    second edit

--- js/baidumap.js
+++ js/baidumap.js
@@ -1,13 +1,12 @@
-    //´´½¨ºÍ³õÊ¼»¯µØÍ¼º¯Êý£º
-    function initMap(){
-      createMap();//´´½¨µØÍ¼
-      setMapEvent();//ÉèÖÃµØÍ¼ÊÂ¼þ
-      addMapControl();//ÏòµØÍ¼Ìí¼Ó¿Ø¼þ
-      addMapOverlay();//ÏòµØÍ¼Ìí¼Ó¸²¸ÇÎï
+﻿function initMap(){
+      createMap();//创建地图
+      setMapEvent();//设置地图事件
+      addMapControl();//向地图添加控件
+      addMapOverlay();//向地图添加覆盖物
     }
     function createMap(){ 
       map = new BMap.Map("map"); 
-      map.centerAndZoom(new BMap.Point(121.53945,31.230819),15);
+      map.centerAndZoom(new BMap.Point(121.539203,31.230552),15);
     }
     function setMapEvent(){
       map.enableScrollWheelZoom();
@@ -22,9 +21,9 @@
     }
     function addMapOverlay(){
       var markers = [
-        {content:"ÉÏº£ÊÐÆÖ¶«ÐÂÇøÊÀ¼Í´óµÀ1500ºÅ527ÊÒ£¨¶«·½´óÏÃ£©",title:"ÉÏº£Ñå¼¯µç×ÓÉÌÎñÓÐÏÞ¹«Ë¾",imageOffset: {width:0,height:3},position:{lat:31.230992,lng:121.539171}}
+        {content:"地址：上海市浦东新区世纪大道1500号527室  电话：02151985787",title:"上海彦集电子商务有限公司",imageOffset: {width:0,height:3},position:{lat:31.231039,lng:121.5391}},
       ];
-      for(var index = 0; index < markers.length; index++ ){s
+      for(var index = 0; index < markers.length; index++ ){
         var point = new BMap.Point(markers[index].position.lng,markers[index].position.lat);
         var marker = new BMap.Marker(point,{icon:new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png",new BMap.Size(20,25),{
           imageOffset: new BMap.Size(markers[index].imageOffset.width,markers[index].imageOffset.height)
@@ -43,12 +42,12 @@
       var labels = [
       ];
       for(var index = 0; index < labels.length; index++){
-        var opt = { position: new BMap.Point(labels[index].position.lng,labels[index].position.lat)};
+        var opt = { position: new BMap.Point(labels[index].position.lng,labels[index].position.lat )};
         var label = new BMap.Label(labels[index].content,opt);
         map.addOverlay(label);
       };
     }
-    //ÏòµØÍ¼Ìí¼Ó¿Ø¼þ
+    //向地图添加控件
     function addMapControl(){
       var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
       scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
